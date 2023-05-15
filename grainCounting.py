939a56859b9a52f7ce81c08bae4139b1dc2b79de
Joshnovski <josh.morphew@hotmail.com>
2023-05-15 10:30:49 +1000

    Implemented a window to have more control over zoom

--- grainCounting.py
+++ grainCounting.py
@@ -1,15 +1,26 @@
+import sys
+
 import cv2
+import sys
 import numpy as np
 
-def count_chocolate_chips(image_path):
+def count_chocolate_chips(image_path, scale_factor):
     # Load the image
     image = cv2.imread(image_path)
+
+    if image is None:
+        print(f"Error: Unable to load image from {image_path}")
+        sys.exit()
+
+    # Resize the image if scale_factor is not 1
+    if scale_factor != 1:
+        image = cv2.resize(image, (int(image.shape[1] * scale_factor), int(image.shape[0] * scale_factor)))
 
     # Convert the image to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Apply a threshold to the grayscale image
-    _, thresholded_image = cv2.threshold(gray_image, 160, 255, cv2.THRESH_BINARY)
+    _, thresholded_image = cv2.threshold(gray_image, 190, 255, cv2.THRESH_BINARY)
 
     # Find contours in the thresholded image
     contours, _ = cv2.findContours(thresholded_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -18,7 +29,7 @@ def count_chocolate_chips(image_path):
     chocolate_chip_contours = []
     for contour in contours:
         area = cv2.contourArea(contour)
-        if area > 60 and area < 6000: # Widen range here to allow for larger particle sizes
+        if 6000 < area < 60000: # Widen range here to allow for larger particle sizes
             chocolate_chip_contours.append(contour)
 
     # Draw the chocolate chip contours on the image
--- main.py
+++ main.py
@@ -1,13 +1,55 @@
+# import cv2
+# from grainCounting import count_chocolate_chips
+#
+# image_path = r"C:\Users\RA user\mgathermal.com\mgathermal.com - Research and Development\3000 Characterisation\Collation Tool - Grain Counting\SI poly 1225-001-01.tiff"
+# scale_factor = 0.1  # Adjust this value to fit the image to your screen
+# chocolate_chip_count, outlined_image, grayscale_image = count_chocolate_chips(image_path, scale_factor)
+#
+# print(f"The number of Al grains visible: {chocolate_chip_count}")
+#
+# cv2.imshow("Grayscale Image", grayscale_image)
+# cv2.imshow("Outlined Grains", outlined_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+#############################################################################################
+
+
 import cv2
+import sys
 from grainCounting import count_chocolate_chips
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Cursor
 
-image_path = r"C:\Users\RA user\mgathermal.com\mgathermal.com - Research and Development\3000 " \
-             r"Characterisation\Collation Tool - Grain Counting\1225-001-01_SEM.png"
-chocolate_chip_count, outlined_image, grayscale_image = count_chocolate_chips(image_path)
 
-print(f"The number of Al grains visible: {chocolate_chip_count}")
+def display_images(grayscale_image_cv, outlined_image_cv):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
-cv2.imshow("Grayscale Image", grayscale_image)
-cv2.imshow("Outlined Chocolate Chips", outlined_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    ax1.imshow(grayscale_image_cv, cmap='gray')
+    ax1.set_title('Grayscale Image')
+    ax1.axis('off')
+    Cursor(ax1, useblit=True, color='red', linewidth=1)
+
+    ax2.imshow(cv2.cvtColor(outlined_image_cv, cv2.COLOR_BGR2RGB))
+    ax2.set_title('Outlined Image')
+    ax2.axis('off')
+    Cursor(ax2, useblit=True, color='red', linewidth=1)
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    image_path = r"C:\Users\RA user\mgathermal.com\mgathermal.com - Research and Development\3000 Characterisation\Collation Tool - Grain Counting\SI poly 1225-001-01.tiff"
+    scale_factor = 1
+    chocolate_chip_count, outlined_image_cv, grayscale_image_cv = count_chocolate_chips(image_path, scale_factor)
+
+    if grayscale_image_cv is not None and outlined_image_cv is not None:
+        print(f"The number of Al grains visible: {chocolate_chip_count}")
+        display_images(grayscale_image_cv, outlined_image_cv)
+    else:
+        print("Error: Unable to process images.")
+        sys.exit()
+
+
+
